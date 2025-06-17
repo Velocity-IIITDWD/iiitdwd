83bc2d06d84719647a9705dd7c8ec93d85f84d71
pratik pakhale <pratikpakhale20@gmail.com>
2025-06-17 16:51:45 +0100

    fix: fix runtime env from gh secrets

--- apps/iiitdwd/src/env/server.ts
+++ apps/iiitdwd/src/env/server.ts
@@ -7,5 +7,9 @@ export const env = createEnv({
     SANITY_DATASET: z.string(),
     SANITY_PROJECT_ID: z.string(),
   },
-  experimental__runtimeEnv: process.env,
+  // For static exports, we need to explicitly map the runtime environment variables
+  runtimeEnv: {
+    SANITY_DATASET: process.env.SANITY_DATASET,
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+  },
 });
